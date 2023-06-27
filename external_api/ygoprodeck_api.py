aeb992b694a5220fbf0e4d4b2d227b11828cdd01
kuroazai <pyromwansa@gmail.com>
2023-06-27 23:16:41 +0100

    external api changes
    tests

--- external_api/ygoprodeck_api.py
+++ external_api/ygoprodeck_api.py
@@ -16,3 +16,13 @@ def get_card_by_archetype(archetype: str) -> dict:
     response = requests.get(route, params=params)
     return response.json()
 
+
+if __name__ == "__main__":
+    # get card by name
+    name = "Chamber Dragonmaid"
+    card = get_card_by_name(name)
+    print(card['data'][0])
+    # get card by archetype
+    archetype = "Blue-Eyes"
+    card = get_card_by_archetype(archetype)
+    print(card['data'][0]['archetype'])
