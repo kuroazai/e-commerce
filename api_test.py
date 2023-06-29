04ea9bfc3d3b189bbce048e13d9c3258e2d3a051
kuroazai <pyromwansa@gmail.com>
2023-06-29 23:33:54 +0100

    Test Endpoint
    cleaned up and added missing routes

--- api_test.py
+++ api_test.py
@@ -1,15 +1,5 @@
-'''
-test authentication
-test get all cards
-test get cards by matching name
-test get cards by matching name and type
-test increase quantity of card with authentication
-test increase quantity of card without authentication
-test exploitation or difference in expected data
-'''
 import unittest
 import requests
-
 
 
 class TestAPI(unittest.TestCase):
@@ -20,10 +10,6 @@ class TestAPI(unittest.TestCase):
         response = requests.get(self.url + route)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
-        pass
-
-    def test_GetFilteredCards(self):
-        pass
 
     def test_GetCard(self):
         route = "/cards/5f9b3b4b9c9b7b3b4c9b7b3b"
@@ -32,7 +18,6 @@ class TestAPI(unittest.TestCase):
         self.assertIsInstance(response.json(), dict)
 
     def test_AddCard(self):
-        # add card route
         route = "/card"
         params = {"name": "Chamber Dragonmaid", "quantity": 1}
         response = requests.post(self.url + route, params=params)
@@ -41,10 +26,19 @@ class TestAPI(unittest.TestCase):
 
     def get_GetFilteredCard(self):
         route = "/card"
-        pass
+        params = {"name": "Chamber Dragonmaid", "type": "Effect Monster"}
+        response = requests.get(self.url + route, params=params)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
 
     def test_UpdateCardQuantity(self):
-        pass
+        route = "/card/5f9b3b4b9c9b7b3b4c9b7b3b"
+        params = {"quantity": 5}
+        response = requests.put(self.url + route, params=params)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Card quantity updated"})
 
     def test_AddCardQuantity(self):
         route = "/card/5f9b3b4b9c9b7b3b4c9b7b3b"
@@ -53,7 +47,3 @@ class TestAPI(unittest.TestCase):
         print(response.json())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "Card quantity updated"})
-
-
-
-
--- external_api/ygoprodeck_api.py
+++ external_api/ygoprodeck_api.py
@@ -1,16 +1,13 @@
 import requests
-import json
 
 
 def get_card_by_name(name: str) -> dict:
-    # get card by name
     route = "https://db.ygoprodeck.com/api/v7/cardinfo.php?"
     params = {"name": name}
     response = requests.get(route, params=params)
     return response.json()
 
 def get_card_by_archetype(archetype: str) -> dict:
-    # get card by archetype
     route = "https://db.ygoprodeck.com/api/v7/cardinfo.php?"
     params = {"archetype": archetype}
     response = requests.get(route, params=params)
@@ -18,11 +15,10 @@ def get_card_by_archetype(archetype: str) -> dict:
 
 
 if __name__ == "__main__":
-    # get card by name
     name = "Chamber Dragonmaid"
     card = get_card_by_name(name)
     print(card['data'][0])
-    # get card by archetype
+
     archetype = "Blue-Eyes"
     card = get_card_by_archetype(archetype)
     print(card['data'][0]['archetype'])
